5020d22427418745c8dcb35b7626f63f1cedba05
Uliana Mykoliuk <ulanasadikova65@gmail.com>
2023-05-22 23:52:16 +0300

    imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,5 @@
 import React from "react";
 import "../styles/index.scss";
-import "../styles/fun-facts.css"
-import "../styles/species-page.css"
-import "../styles/circle-card.css"
-import "../styles/footer.css"
-import "../styles/header.css"
-import "../styles/main-background.css"
-import "../styles/photo-card.css"
-import "../styles/text-image-letter.css"
 
 
 export default function MyApp({ Component, pageProps }) {
